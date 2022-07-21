8984b2dcc201a2c7fb22e5a99e68f6cd4d95edea
Misha Statnik <mishastatnik@gmail.com>
2022-07-21 19:38:14 +0300

    Add game_session.hpp
    class GameSession contains all necessary data about a game
    class Games contains all games and has methods to manipulate these games

---
+++ Battleships/game_session.hpp
@@ -0,0 +1,38 @@
+#ifndef GAME_SESSION_HPP_SENTRY
+#define GAME_SESSION_HPP_SENTRY
+#include <vector>
+
+struct GameSession {
+    int p1id;
+    int p2id;
+    int p1field [10][10]; // 0 means nothing, 1 there is a ship
+    int p2field [10][10];
+    bool p1turn = true;
+    bool game_end = false;
+    bool p1winner;
+    GameSession(int _p1id, int _p2id) :
+        p1id(_p1id), p2id(_p2id) {}
+};
+ 
+// class to manipulate all games silmutaneously   
+class Games {
+    int last_game_id;
+public:
+    Games() : last_game_id(0) {}
+    std::vector<GameSession> games;
+    int hit(int y, int x, int pid);
+    // -1 - not your turn
+    // 0  - miss
+    // 1  - strike
+
+    void addGame(int p1id, int p2id); 
+    int removeGame(int pid);
+
+    int gameEnded(int pid); 
+    // 0 - game hasn't ended
+    // <id> - id of player who won
+};
+
+
+
+#endif
