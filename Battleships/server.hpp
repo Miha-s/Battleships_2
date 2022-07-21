8e48da546fc4a867676213e609be0a50775ead57
Misha Statnik <mishastatnik@gmail.com>
2022-07-21 18:01:02 +0300

    Add server.hpp
    class ChatSession is designed to read messages from user and keep information about this user
    class Server is designed to manipulate all ongoing games and users

---
+++ Battleships/server.hpp
@@ -0,0 +1,54 @@
+#ifndef SERVER_HPP_SENTRY
+#define SERVER_HPP_SENTRY
+#include <list>
+
+#include "sockets.hpp"
+
+
+enum {
+    max_line_length = 1023,
+    qlen_for_listen = 16
+};
+
+class Server;
+
+class ChatSession : FdHandler {
+    friend class Server;
+
+    char buffer [max_line_length + 1];
+    int buf_used;
+    bool ignoring;
+
+    int id;
+
+    Server *the_master;
+
+    ChatSession(Server *a_master, int fd);
+    ~ChatSession();
+
+    void Send(const char *msg);
+
+    virtual void Handle(bool r, bool w);
+
+    void ReadAndIgnore();
+    void ReadAndCheck();
+    void CheckMessage();
+};
+
+class Server : public FdHandler {
+    EventSelector *the_selector;
+    std::list <ChatSession*> sessions;
+
+
+    Server(EventSelector *sel, int fd);
+public:
+    ~Server();
+    static Server *Start(EventSelector *sel, int port);
+
+    void RemoveSession(ChatSession *s);
+
+private:
+    virtual void Handle(bool r, bool w);
+};
+
+#endif
