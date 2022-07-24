0de0d8cca0eb7fd76ddc1d5abcb7a7dd9b690d4d
Misha Statnik <mishastatnik@gmail.com>
2022-07-24 18:57:13 +0300

    Add error checking in sendfile

--- src/server.cpp
+++ src/server.cpp
@@ -139,7 +139,8 @@ void Server::ProcessMessage(char *str, ChatSession* ses)
     const char* buf = response_header.c_str();
     int sd = ses->GetFd();
     write(sd, buf, strlen(buf));
-    sendfile(sd, fd, NULL, size);
+    int err = sendfile(sd, fd, NULL, size);
+    std::cout << err << std::endl;
     close(fd);
 }
 
